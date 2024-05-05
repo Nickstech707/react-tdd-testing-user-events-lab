a4545f4c006614183f58a5409b9f5fc3012b1afb
Nicholas Muriithi <nickstech707@gmail.com>
2024-05-05 21:29:03 +0300

    Uses test-driven development to create a form and test form events

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+import userEvent from "@testing-library/user-event";
 
 import App from "../App";
 
@@ -66,26 +67,50 @@ test("displays the correct links", () => {
 
 // Newsletter Form - Initial State
 test("the form includes text inputs for name and email address", () => {
-  // your test code here
+  render(<App />);
+
+  expect(screen.getByLabelText(/enter your name/i)).toBeInTheDocument();
+  expect(
+    screen.getByLabelText(/enter your email address/i)
+  ).toBeInTheDocument();
 });
 
 test("the form includes three checkboxes to select areas of interest", () => {
-  // your test code here
+  render(<App />);
+
+  expect(screen.getAllByRole("checkbox").length).toBe(3);
 });
 
 test("the checkboxes are initially unchecked", () => {
-  // your test code here
+  render(<App />);
+
+  expect(
+    screen.getByRole("checkbox", { name: /interest 1/i })
+  ).not.toBeChecked();
+  expect(
+    screen.getByRole("checkbox", { name: /interest 2/i })
+  ).not.toBeChecked();
+  expect(
+    screen.getByRole("checkbox", { name: /interest 3/i })
+  ).not.toBeChecked();
+});
+
+test("the form includes a Submit button", () => {
+  render(<App />);
+
+  expect(screen.getByRole("button", { name: /submit/i })).toBeInTheDocument();
 });
 
 // Newsletter Form - Adding Responses
 test("the page shows information the user types into the name and email address form fields", () => {
-  // your test code here
-});
+  render(<App />);
 
-test("checked status of checkboxes changes when user clicks them", () => {
-  // your test code here
-});
+  const fullName = screen.getByLabelText(/enter your name/i);
+  const emailAddress = screen.getByLabelText(/enter your email address/i);
 
-test("a message is displayed when the user clicks the Submit button", () => {
-  // your test code here
+  userEvent.type(fullName, "Fname Lname");
+  userEvent.type(emailAddress, "fnamelname@email.com");
+
+  expect(fullName).toHaveValue("Fname Lname");
+  expect(emailAddress).toHaveValue("fnamelname@email.com");
 });
